19a37560641bfb0e4da9e15c7f3e5b47007d8176
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-23 16:01:51 +0300

    Fix syntax error in urls.py

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', include('myapp.api.urls')),
     path('api/auth/', include('rest_framework.urls')),  # DRF browsable login
     path('api/', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
